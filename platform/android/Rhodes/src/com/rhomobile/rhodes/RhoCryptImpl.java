25ab5c316fb196c08202531d07c9176857729d9f
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-08-01 15:13:23 -0500

    applying android db fix from 2-4-stable

--- platform/android/Rhodes/src/com/rhomobile/rhodes/RhoCryptImpl.java
+++ platform/android/Rhodes/src/com/rhomobile/rhodes/RhoCryptImpl.java
@@ -1,29 +1,3 @@
-/*------------------------------------------------------------------------
-* (The MIT License)
-* 
-* Copyright (c) 2008-2011 Rhomobile, Inc.
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-* 
-* http://rhomobile.com
-*------------------------------------------------------------------------*/
-
 package com.rhomobile.rhodes;
 
 import java.security.SecureRandom;
@@ -36,14 +10,16 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.ByteBuffer;
 
+//import android.util.Base64;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.app.Activity;
 import com.rhomobile.rhodes.Logger;
-import com.rhomobile.rhodes.util.ContextFactory;
 
 public class RhoCryptImpl 
 {
     private static final String TAG = "RhoCryptJava";
+    private static final boolean DEBUG = false;
 
 	byte[] m_dbKeyData;
 	String m_strDBPartition;
@@ -53,13 +29,29 @@ public class RhoCryptImpl
 
     private void readKeyFromStorage()
     {
-        SharedPreferences settings = ContextFactory.getContext().getSharedPreferences( m_strPrefName, Context.MODE_PRIVATE);
-    	String strOldKey = settings.getString(m_strDBPartition, "");
-    	if ( strOldKey != null && strOldKey.length() > 0 )
-    	{
-    		m_dbKeyData = Base64.decode(strOldKey);//, Base64.DEFAULT);
-    	}
-	}
+        SharedPreferences settings = RhodesService.getInstance().getSharedPreferences( m_strPrefName, Context.MODE_PRIVATE);
+        String strOldKey = settings.getString(m_strDBPartition, "");
+        if (strOldKey != null && strOldKey.length() > 0)
+        {
+            
+            m_dbKeyData = Base64.decode(strOldKey);//, Base64.DEFAULT);
+            Logger.I(TAG, "Key is successfully read from Shared Preferences");
+            if(DEBUG) {
+                Logger.I(TAG, "Key: " + dumpKey(m_dbKeyData));
+                Logger.I(TAG, "Encoded key: " + strOldKey);
+            }
+        } else {
+            Logger.I(TAG, "No key is found in Shared Preferences");
+        }
+    }
+
+    private static String dumpKey(byte[] key) {
+        StringBuffer buffer = new StringBuffer();
+        for(byte b:key) {
+            buffer.append(String.format("%x ", b));
+        }
+        return buffer.toString();
+    }
 
 	private void generateNewKey()throws Exception
 	{
@@ -68,15 +60,28 @@ public class RhoCryptImpl
 	    kgen.init(m_nKeyLenBit, sr);  
 	    SecretKey skey = kgen.generateKey();  
 	    m_dbKeyData = skey.getEncoded();
+	    Logger.I(TAG, "New key is generated");
+	    if(DEBUG)
+	        Logger.I(TAG, "Key: " + dumpKey(m_dbKeyData));
 	}
 
-	private void writeKeyToStorage()
-	{
-		SharedPreferences settings =  ContextFactory.getContext().getSharedPreferences(m_strPrefName, Context.MODE_PRIVATE);		
-    	SharedPreferences.Editor editor = settings.edit();
-    	String strKey = Base64.encodeToString(m_dbKeyData, false);//, Base64.DEFAULT);
-    	editor.putString(m_strDBPartition, strKey );
-    	editor.commit();
+    private void writeKeyToStorage()
+    {
+        SharedPreferences settings = RhodesService.getInstance().getSharedPreferences(m_strPrefName, Context.MODE_PRIVATE);		
+        SharedPreferences.Editor editor = settings.edit();
+        String strKey = Base64.encodeToString(m_dbKeyData, false);//, Base64.DEFAULT);
+        
+        editor.putString(m_strDBPartition, strKey );
+        if (editor.commit()) {
+            Logger.I(TAG, "Key is successfully saved to Shared Preferences");
+        } else {
+            Logger.E(TAG, "Write key to Shared Preferences is failed");
+        }
+
+        if (DEBUG) {
+            Logger.I(TAG, "Key: " + dumpKey(m_dbKeyData));
+            Logger.I(TAG, "Encoded key: " + strKey);
+        }
 	}
 
 	private void initKey(String szPartition)throws Exception
@@ -118,35 +123,38 @@ public class RhoCryptImpl
 		m_decryptCipher.init(Cipher.DECRYPT_MODE, skeySpec, paramSpec);
 		
         Logger.T(TAG, "RhoCrypt context initialized for partition: " + szPartition);
+    }
 
-	}
-	
-	private static void reportFail(String name, Exception e) 
-	{
-	    Logger.E(TAG, "Call of \"" + name + "\" failed: " + e.getClass().getSimpleName() + ": " + e.getMessage());
-	}
-	
     public boolean db_encrypt( String szPartition, ByteBuffer dataIn, ByteBuffer dataOut )
     {
-    	try
-    	{
-	    	initContext(szPartition);
+        try
+        {
+            initContext(szPartition);
 
             dataOut.rewind();
 
             if (m_encryptCipher == null)
                 throw new NullPointerException("m_encryptCipher == null");
 
-            m_encryptCipher.doFinal(dataIn, dataOut);
+            /* *********************************************************
+             * Work around Android 4 bug:
+             * http://code.google.com/p/android/issues/detail?id=24327 
+             */
+            ByteBuffer input = ByteBuffer.allocate(dataIn.capacity());
+            input.put(dataIn);
+            input.rewind();
+            /* ******************************************************* */
 
-	    	return true;
-    	}catch(Exception exc)
-    	{
-    		reportFail("db_encrypt", exc);
-    		return false;
-    	}
+            m_encryptCipher.doFinal(input, dataOut);
+
+            return true;
+        }catch(Exception exc)
+        {
+            Logger.E(TAG, exc);
+            return false;
+        }
     }
-    
+
     public boolean db_decrypt( String szPartition, ByteBuffer dataIn, ByteBuffer dataOut )
     {
         try
@@ -158,12 +166,21 @@ public class RhoCryptImpl
             if (m_decryptCipher == null)
                 throw new NullPointerException("m_decryptCipher == null");
 
-            m_decryptCipher.doFinal(dataIn, dataOut);
+            /* *********************************************************
+             * Work around Android 4 bug:
+             * http://code.google.com/p/android/issues/detail?id=24327 
+             */
+            ByteBuffer input = ByteBuffer.allocate(dataIn.capacity());
+            input.put(dataIn);
+            input.rewind();
+            /* ******************************************************* */
+
+            m_decryptCipher.doFinal(input, dataOut);
 
             return true;
 		}catch(Exception exc)
 		{
-			reportFail("db_decrypt", exc);
+			Logger.E(TAG, exc);
 			return false;
 		}
     }
@@ -175,7 +192,7 @@ public class RhoCryptImpl
 	    
 	    if ( keyData.length != (m_nKeyLenBit/8))
 	    {
-	    	reportFail("set_db_CryptKey", new Exception("Incorrect key size : " + keyData.length + "; Should be: " + (int)(m_nKeyLenBit/8)));
+	    	Logger.E(TAG, "Incorrect key size : " + keyData.length + "; Should be: " + (int)(m_nKeyLenBit/8));
 	    	return false;
 	    }
 	    m_dbKeyData = keyData;
